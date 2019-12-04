9770e019b2e15dc93d156056bbae70d56d88b443
lvcha <997027407@qq.com>
2019-12-04 15:04:52 +0800

    helloworld

--- app/src/main/java/com/zs/rxjavademo_p1/MainActivity.java
+++ app/src/main/java/com/zs/rxjavademo_p1/MainActivity.java
@@ -2,6 +2,12 @@ package com.zs.rxjavademo_p1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void test(View view){
+        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+            }
+        });
+        observable.subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                System.out.println(integer);
+            }
+        });
     }
 }
