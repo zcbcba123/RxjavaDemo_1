74085db8b56375162b039b2e5c2ff758ea8fe040
lvcha <997027407@qq.com>
2019-12-04 17:51:56 +0800

    增加interval和range

--- app/src/main/java/com/zs/rxjavademo_p1/MainActivity.java
+++ app/src/main/java/com/zs/rxjavademo_p1/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
@@ -28,6 +30,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         observable.subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                System.out.println(integer);
+            }
+        });
+    }
+    
+    public void interval(View view){
+        Observable.interval(5,3, TimeUnit.SECONDS).subscribe(new Consumer<Long>() {
+            @Override
+            public void accept(Long aLong) throws Exception {
+                System.out.println(aLong);
+            }
+        });
+    }
+    public void range(View view){
+        Observable.range(5,10).subscribe(new Consumer<Integer>() {
             @Override
             public void accept(Integer integer) throws Exception {
                 System.out.println(integer);
