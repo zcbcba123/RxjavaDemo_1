1fc6706c1663092857ae0ee54358cc027e06549c
lvcha <997027407@qq.com>
2019-12-17 13:44:28 +0800

    ~~

--- app/src/main/java/com/zs/rxjavademo_p1/MainActivity.java
+++ app/src/main/java/com/zs/rxjavademo_p1/MainActivity.java
@@ -44,6 +44,10 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println(aLong);
             }
         });
+//        Observable.intervalRange()
+//        intervalRange方法可以用来将发射的整数序列限制在一个范围之内，
+//        这里的start用来表示发射的数据的起始值，
+//        count表示总共要发射几个数字，其他参数与上面的interval方法一致。
     }
     public void range(View view){
         Observable.range(5,10).subscribe(new Consumer<Integer>() {
@@ -52,5 +56,30 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println(integer);
             }
         });
+//        rangeLong
+//        这里的两个参数start用来指定用于生成的序列的开始值，count用来指示要生成的序列总共包含多少个数字，
+//        上面的两个方法的主要区别在于一个是用来生成int型整数的，一个是用来生成long型整数的。
     }
+
+    public void create(View view){
+        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onComplete();
+            }
+        });
+        observable.subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                System.out.println(integer);
+            }
+        });
+    }
+
+
+
+
+
 }
